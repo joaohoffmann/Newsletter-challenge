0e806982c2d6c5d0eea272f6e451099540ca5452
João Pedro Hoffmann <joao.pedro.jordao09@gmail.com>
2022-07-08 15:20:47 -0300

    third commit

---
+++ script.js
@@ -0,0 +1,10 @@
+btn.addEventListener('click', (event)=>{
+  modal__container.style.display="flex"
+  event.preventDefault()
+})
+
+X.addEventListener('click', (event)=>{
+  modal__container.style.display="none"
+  event.preventDefault()
+})
+
